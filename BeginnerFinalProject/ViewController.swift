4b18b922cde906264d027e750c1ba393a12b1bce
Michael Murphy <mpmurphy1432@gmail.com>
2018-06-29 17:43:46 -0500

    Initial Commit

---
+++ BeginnerFinalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeginnerFinalProject
+//
+//  Created by Michael  Murphy on 6/29/18.
+//  Copyright © 2018 Michael  Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
